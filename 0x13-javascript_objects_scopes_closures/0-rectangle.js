cda0ba5fc3ea3e227b05385ee19ed34ee241a5c1
root <odosolomonchi@gmail.com>
2023-04-12 10:00:59 +0000

    task

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//The class is empty(class Rectangle)defining a rectangle
+class Rectangle {
+  constructor() {}
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+This repo contains solution to task 0x13. Javascript - Objects, Scopes and Closures
